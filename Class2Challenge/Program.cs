5b79642725f416365dbc13037137f694a2742122
Tim Lucas <tllucas1s@gmail.com>
2018-05-12 15:02:15 -0500

    Finished challenge.

--- Class2Challenge/Program.cs
+++ Class2Challenge/Program.cs
@@ -10,6 +10,36 @@ namespace Class2Challenge
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter a string: ");
+            string s = Console.ReadLine();
+            bool complete = false;
+
+            while (complete == false)
+            {
+                if (s.Length > 1)
+                {
+                    for (int i = 1; i < s.Length;)
+                    {
+                        if (s[i] == s[i - 1])
+                        {
+                            s = s.Remove(i - 1, 2);
+                            i = 1;
+                        }
+                        else
+                        { 
+                            i = i+1;
+                        }
+                    }
+                    complete = true;
+                }
+                else
+                {
+                    complete = true;
+                }
+            }
+
+            Console.WriteLine("Your result = " + s);
+            Console.ReadLine();
         }
     }
 }
